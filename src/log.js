7234972af3b41b3e446933b21a03450322bcb063
luyong <luyong@nationalchip.com>
2018-01-31 12:44:51 +0800

    fix: 解决HDGO无法工作

--- src/log.js
+++ src/log.js
@@ -1,51 +1,55 @@
-console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
-console.log(service.debug)
-console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
-
-
-var depth =''
 function logData(data, logFunction, depth) {
-  if (!depth) depth = 0;
+    if (!depth) depth = 0;
 
-  var body = "";
-  for (var i = 0; i < depth; i++) body += "  ";
-  if (depth > 0) body += "'-> ";
+    var body = '';
+    for (var i = 0; i < depth; i++) body += '  ';
+    if (depth > 0) body += "'-> ";
 
-  if (data === null) logFunction(body + "NULL");
-  else if (data === undefined) logFunction(body + "UNDEFINED");
-  else if (typeof (data) === 'boolean') logFunction(body + data);
-  else if (typeof (data) === 'number') logFunction(body + data);
-  else if (typeof (data) === 'string') logFunction(body + data);
-  else if (Array.isArray(data)) logFunction(JSON.stringify(data, null, 4));
-  else if (typeof (data) === 'object') logFunction(JSON.stringify(data, null, 4));
-  else if (typeof (data) === 'function') logFunction(body + "Function()");
-  else logFunction(body + "Unknown data type");
+    if (data === null) logFunction(body + 'NULL');
+    else if (data === undefined) logFunction(body + 'UNDEFINED');
+    else if (typeof data === 'boolean') logFunction(body + data);
+    else if (typeof data === 'number') logFunction(body + data);
+    else if (typeof data === 'string') logFunction(body + data);
+    else if (Array.isArray(data)) logFunction(JSON.stringify(data, null, 4));
+    else if (data instanceof ReferenceError) console.error(body + data.stack);
+    else if (typeof data === 'object') logFunction(JSON.stringify(data, null, 4));
+    else if (typeof data === 'function') logFunction(body + 'Function()');
+    else logFunction(body + 'Unknown data type');
 }
 
-exports.d = function (data) {
-  if (service.debug) logData(data, console.log);
+exports.d = function(data) {
+    if (service.debug) logData(data, console.error);
 };
 
-exports.e = function (data) {
-  if (service.debug) logData(data, console.error);
+exports.e = function(data) {
+    if (service.debug) logData(data, console.error);
 };
 
-exports.p = function (data) {
-  if (service.debug) logData(data, print);
+exports.p = function(data) {
+    if (service.debug) logData(dump(data), console.error);
 };
 
-exports.dump = function dump(c, depth) {
-    var a = "";
-    depth || (depth = 0);
-    for (var e = "", b = 0; b < depth + 1; b++) {
-        e += "    ";
+function dump(arr, level) {
+    var dumped_text = '';
+    if (!level) {
+        level = 0;
     }
-    if ("object" == typeof c) {
-        for (var f in c) {
-            b = c[f], "object" == typeof b ? (a += e + "'" + f + "' ...\n", a += dump(b, depth + 1)) : a += e + "'" + f + "' => \"" + b + '"\n';
+    var level_padding = '';
+    for (var j = 0; j < level + 1; j++) {
+        level_padding += '    ';
+    }
+    if (typeof arr == 'object') {
+        for (var item in arr) {
+            var value = arr[item];
+            if (typeof value == 'object') {
+                dumped_text += level_padding + "'" + item + "' ...\n";
+                dumped_text += dump(value, level + 1);
+            } else {
+                dumped_text += level_padding + "'" + item + '\' => "' + value + '"\n';
+            }
         }
     } else {
-        a = "===>" + c + "<===(" + typeof c + ")";
+        dumped_text = '===>' + arr + '<===(' + typeof arr + ')';
     }
-    return (service.debug)? console.log(a) : ''
+    return dumped_text;
 }
